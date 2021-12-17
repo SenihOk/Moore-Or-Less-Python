754edce2fb02505bfff3cd2b1f1a9040088c8e2b
SenihOk <Senih.okuyucu@gmail.com>
2021-12-16 21:28:57 -0500

    removed boto

--- apiCodes.py
+++ apiCodes.py
@@ -1,5 +1,4 @@
 import os
-from boto.s3.connection import S3Connection
 
 class Codes:
     def __init__(self):
@@ -20,9 +19,9 @@ class Codes:
         args: none
         return: none
         """
-        self.api_key = S3Connection(os.environ['API_KEY'])
-        self.api_key_scrt = S3Connection(os.environ['API_KEY_SECRET'])
-        self.bearer_tkn = S3Connection(os.environ['BEARER_TOKEN'])
-        self.access_tkn = S3Connection(os.environ['ACCESS_TOKEN'])
-        self.access_tkn_scrt = S3Connection(os.environ['ACCESS_TOKEN_SECRET'])
+        self.api_key = (os.environ['API_KEY'])
+        self.api_key_scrt = (os.environ['API_KEY_SECRET'])
+        self.bearer_tkn = (os.environ['BEARER_TOKEN'])
+        self.access_tkn = (os.environ['ACCESS_TOKEN'])
+        self.access_tkn_scrt = (os.environ['ACCESS_TOKEN_SECRET'])
         return {"api_key": self.api_key, "api_key_scrt": self.api_key_scrt, "access_tkn": self.access_tkn, "access_tkn_scrt" : self.access_tkn_scrt}
